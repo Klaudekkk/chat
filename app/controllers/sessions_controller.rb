4094fd41f653b2dc6c8377e2f299830ed842f9dd
Klaudia Wojciechowska <kwojciechowska@edu.cdv.pl>
2022-08-12 19:22:23 +0200

    Log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,21 @@ class SessionsController <ApplicationController
 
   end
 
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session["user_id"] = user.id
+      flash[:success] = "Zalogowano się"
+      redirect_to root_path
+    else
+      flash.now[:error] = "Nie mozna sie zalogować"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Wylogowano użytkownika"
+    redirect_to login_path
+  end
 end
